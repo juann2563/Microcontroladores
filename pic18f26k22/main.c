5d9f8df2e51180a025f31c5ea9fa1d640840710f
juann2563 <juanzabala2193@gmail.com>
2018-02-08 22:06:55 -0500

    added duty tester

--- pic18f26k22/main.c
+++ pic18f26k22/main.c
@@ -1,19 +1,24 @@
 #include <18f26K22.h>
 
-#fuses NOWDT, NOMCLR, PLLEN
+#fuses NOWDT, NOMCLR
 #use delay(clock=64MHz, oscillator=16MHz)
-#byte TRISB = 0xf93
-#byte PORTB = 0xf81
-#byte LATB = 0xf8a
-#bit RB0 = LATB.0
 
 void main(){
-   TRISB=0b00000000;
-   while(true){
-      RB0 = 1;
-      delay_ms(1000);
-      RB0 = 0;
-      delay_ms(1000);
+	int tmrx = 1;
+	int prx = 159;
+	int duty = 0;
+	set_tris_c(0x00);
+	setup_timer_2(t2_div_by_1,prx,1);
+	setup_ccp1(ccp_pwm);
+        
+   while(true)
+   {
+      set_pwm1_duty(duty);
+      duty = duty+20;
+      delay_ms(500);
+      if(duty >= 639){
+    	duty=0;
+      }
       
-   }
+   } 
 }
