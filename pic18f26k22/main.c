0b34dbd6e57191d1bd53a70d89981def034fdfa8
juann2563 <juanzabala2193@gmail.com>
2018-02-08 22:34:21 -0500

    addd adc read

--- pic18f26k22/main.c
+++ pic18f26k22/main.c
@@ -1,24 +1,52 @@
 #include <18f26K22.h>
-
-#fuses NOWDT, NOMCLR
-#use delay(clock=64MHz, oscillator=16MHz)
+#device adc=10;     
+#fuses NOWDT, NOMCLR, HSH  
+#use delay(clock=64MHz, oscillator=16MHz)     // para configurar a 64MHz con pll
 
 void main(){
 	int tmrx = 1;
 	int prx = 159;
-	int duty = 0;
+	long duty = 0;
 	set_tris_c(0x00);
 	setup_timer_2(t2_div_by_1,prx,1);
 	setup_ccp1(ccp_pwm);
+	setup_adc(adc_clock_internal);
+	setup_adc_ports(all_analog);                         
         
    while(true)
-   {
-      set_pwm1_duty(duty);
-      duty = duty+20;
-      delay_ms(500);
-      if(duty >= 639){
-    	duty=0;
-      }
-      
+   {                              
+
+		long value;
+ 
+		 
+		set_adc_channel(0);
+		 //the next read_adc call will read channel 0
+		 
+		delay_us(10);
+		 //a small delay is required after setting the channel
+		 
+		 
+		 //and before read
+		 
+		value=read_adc();
+		 //starts the conversion and reads the result
+		 
+		 
+		 //and store it in value
+		 
+		read_adc(ADC_START_ONLY);
+		 //only starts the conversion
+		 
+		value=read_adc(ADC_READ_ONLY);
+		duty = value;
+ 
+   
+	    set_pwm1_duty(duty);     
+	    duty = duty+20;            
+	    delay_ms(500);
+	    if(duty >= 639)
+	    {
+	    	duty=0;
+	    } 
    } 
 }
